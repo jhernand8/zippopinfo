c1bfaa1fafb99e5b9471c7e01267207599e3dcab
Jonathan Hernandez <jbhernan@gmail.com>
2017-10-29 14:17:46 -0700

    Added models

--- census/models.py
+++ census/models.py
@@ -3,3 +3,17 @@ from django.db import models
 # Create your models here.
 class Greeting(models.Model):
     when = models.DateTimeField('date created', auto_now_add=True)
+
+# Census data information type. Contains the key used by the
+# census api and a description of the data for that key.
+class InfoType(models.Model):
+    census_key = models.
+    info_type_id = models.AutoField(primary_key = True)
+    description = models.TextField()
+
+# Data for actual census data for a given zip code.
+class CensusInfo(models.Model):
+    info_type_id = models.IntegerField()
+    census_info_id = models.AutoField(primary_key = True)
+    info = models.IntegerField()
+    zipCode = models.TextField()
