09d772ab873d3acd3772353764afde5a85e15dac
Jonathan Hernandez <jbhernan@gmail.com>
2017-10-29 22:07:11 -0700

    CL

---
+++ census/management/commands/CLTest.py
@@ -0,0 +1,17 @@
+from django.core.management.base import BaseCommand, CommandError
+from census import censusDataUtils
+from census.models import InfoType
+from census.models import CensusInfo
+
+class Command(BaseCommand):
+
+  def handle(self, *args, **options):
+    zips = ['94010', '94131', '94404'];
+    apiKey = os.environ['CENSUS_API_KEY']
+    infoTypes = InfoType.objects.all()
+
+    for zip in zips:
+      data = censusDataUtils.fetchAndFormCensusInfoForZip(zip, 6, apiKey, infoTypes)
+      
+      print str(d.info) + ": " + zip + "\n";
+
