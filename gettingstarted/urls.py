21ff5d465b3545b9de5e6ff943f50e1980ba54ea
Jonathan Hernandez <jbhernan@gmail.com>
2017-10-29 17:13:32 -0700

    urls

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -13,5 +13,5 @@ urlpatterns = [
     url(r'^$', census.views.index, name='index'),
     url(r'^db', census.views.db, name='db'),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^addZip$' 'census.views.add_zip'),
+    url(r'^addZip$', census.views.add_zip, name='zips'),
 ]
